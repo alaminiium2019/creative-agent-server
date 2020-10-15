c9c9f1ce3f00157d328a9c02af684d589985fe09
alaminiium2019 <alamin.iium2017@gmail.com>
2020-10-15 06:15:26 +0600

    photo uploaded

--- index.js
+++ index.js
@@ -37,14 +37,40 @@ client.connect(err => {
 
     //Add review
 
+    // app.post('/addreview',(req,res) => {
+    //     const reviews = req.body;
+    //     console.log(reviews);
+    //     reviewCollection.insertOne(reviews)
+    //     .then(result => {
+    //         res.send(result.insertedCount>0)
+    //     })
+    // })
+
+    //add review
+
     app.post('/addreview',(req,res) => {
-        const reviews = req.body;
-        console.log(reviews);
-        reviewCollection.insertOne(reviews)
-        .then(result => {
-            res.send(result.insertedCount>0)
-        })
-    })
+        const file = req.files.file;
+        const name = req.body.name;
+        const company = req.body.company;
+        const description = req.body.description;
+        const newImg = file.data;
+        const encImg =newImg.toString('base64');
+    
+            var image ={
+                // contentType: req.files.file.mimetype,
+                contentType: file.mimetype,
+                // size:req.files.file.size,
+                size:file.size,
+                img: Buffer.from(encImg,'base64')
+            };
+    
+            reviewCollection.insertOne({name,company,description,image})
+            .then(result =>{
+                    res.send(result.insertedCount>0)    
+                })
+               
+            })
+            
 
 //Add orders
     app.post('/addorders', (req, res) => {
@@ -64,18 +90,6 @@ app.post('/adminAddService',(req,res) => {
     const details = req.body.details;
     const newImg = file.data;
     const encImg =newImg.toString('base64');
-
-    // console.log(name,details,file);
-    // const filePath = `${__dirname}/creatives/${file.name}`;
-
-
-    // file.mv(`${__dirname}/creatives/${file.name}`,err =>{
-    //     if(err){
-    //         console.log(err);
-    //         res.status(500).send({msg: 'Failed to upload Image'});
-    //     }
-    //     var newImg =fs.readFileSync(filePath);
-    //     const encImg = newImg.toString('base64');
 
         var image ={
             // contentType: req.files.file.mimetype,
@@ -120,6 +134,26 @@ app.get('/getService',(req,res) =>{
 
 })
 
+//get review
+
+app.get('/getReview',(req,res) =>{
+    reviewCollection.find({})
+    .toArray((err,documents) => {
+        res.send(documents)
+    })
+
+})
+
+//admin
+
+
+// app.post('/isDoctor', (req, res) => {
+//     const email = req.body.email;
+//     collection.find({ email: email })
+//         .toArray((err, doctors) => {
+//             res.send(doctors.length > 0);
+//         })
+// })
     console.log('db is connected')
 
 });
